aaa121bac12eb9b49061d303719898abdb55bd17
burno <bruno.wieczorek@gmail.com>
2020-03-13 17:04:11 +0100

    Add try-except

--- app.py
+++ app.py
@@ -9,29 +9,33 @@ driver = None
 
 while True:
     print("### fetching new links")
-    links = get_links(interval_minutes)
-    print("### number of matching links: " + str(len(links)))
-    for link in links:
-        print("### title:", link.title)
-        print("### url:", link.source_url)
-        driver = create_driver()
-        article = get_article(driver, link.source_url)
-        if article:
-            translation = get_translation(driver, article)
-            if translation:
-                print(translation)
-                # TODO: add_comment(link.id, "tresc")
+    try:
+        links = get_links(interval_minutes)
+        print("### number of matching links: " + str(len(links)))
+        for link in links:
+            print("### title:", link.title)
+            print("### url:", link.source_url)
+            driver = create_driver()
+            article = get_article(driver, link.source_url)
+            if article:
+                translation = get_translation(driver, article)
+                if translation:
+                    print(translation)
+                    # TODO: add_comment(link.id, "tresc")
+                else:
+                    print(
+                        "###",
+                        "polish article"
+                        if translation == None
+                        else "translation not found (timeout?)",
+                    )
             else:
-                print(
-                    "###",
-                    "polish article"
-                    if translation == None
-                    else "translation not found (timeout?)",
-                )
-        else:
-            print("### scraping article failed")
-        driver.close()
-    if driver:
-        driver.quit()
+                print("### scraping article failed")
+            driver.close()
+        if driver:
+            driver.quit()
+    except Exception as e:
+        print("### something went wrong:")
+        print(str(e))
     print("### sleeping...")
     time.sleep(interval_minutes * 60)
