d3d330c016ba8d6d998fa8dc0536e386d9713c96
burno <bruno.wieczorek@gmail.com>
2020-03-13 17:13:10 +0100

    Refactor try-exceot

--- app.py
+++ app.py
@@ -7,35 +7,40 @@ import time
 interval_minutes = 3
 driver = None
 
+def handle_links(links):
+    for link in links:
+        print("### title:", link.title)
+        print("### url:", link.source_url)
+        driver = create_driver()
+        article = get_article(driver, link.source_url)
+        if article:
+            translation = get_translation(driver, article)
+            if translation:
+                print(translation)
+                # TODO: add_comment(link.id, "tresc")
+            else:
+                print(
+                    "###",
+                    "polish article"
+                    if translation == None
+                    else "translation not found (timeout?)",
+                )
+        else:
+            print("### scraping article failed")
+        driver.close()
+
 while True:
     print("### fetching new links")
     try:
         links = get_links(interval_minutes)
         print("### number of matching links: " + str(len(links)))
-        for link in links:
-            print("### title:", link.title)
-            print("### url:", link.source_url)
-            driver = create_driver()
-            article = get_article(driver, link.source_url)
-            if article:
-                translation = get_translation(driver, article)
-                if translation:
-                    print(translation)
-                    # TODO: add_comment(link.id, "tresc")
-                else:
-                    print(
-                        "###",
-                        "polish article"
-                        if translation == None
-                        else "translation not found (timeout?)",
-                    )
-            else:
-                print("### scraping article failed")
-            driver.close()
-        if driver:
-            driver.quit()
+        handle_links(links)
     except Exception as e:
         print("### something went wrong:")
         print(str(e))
+    finally:
+        if driver:
+            driver.quit()
+            driver = None
     print("### sleeping...")
     time.sleep(interval_minutes * 60)
