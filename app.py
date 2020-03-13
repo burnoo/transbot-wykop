17757b775b66ff5c0cf4ccae31968f4d03db3a13
burno <bruno.wieczorek@gmail.com>
2020-03-13 18:50:52 +0100

    Add logging to file

--- app.py
+++ app.py
@@ -7,41 +7,47 @@ import time
 interval_minutes = 3
 driver = None
 
+def log(*messages):
+    log = open("transbot.log", "a")
+    for message in messages:
+        log.write(message + "\n")
+        print(message)
+
 def handle_links(links):
     for link in links:
-        print("### title:", link.title)
-        print("### url:", link.source_url)
+        log("### title:", link.title)
+        log("### url:", link.source_url)
         driver = create_driver()
         article = get_article(driver, link.source_url)
         if article:
             translation = get_translation(driver, article)
             if translation:
-                print(translation)
+                log(translation)
                 add_comment(link.id, translation)
             else:
-                print(
+                log(
                     "###",
                     "polish article"
                     if translation == None
                     else "translation not found (timeout?)",
                 )
         else:
-            print("### scraping article failed")
+            log("### scraping article failed")
         driver.close()
 
 while True:
-    print("### fetching new links")
+    log("### fetching new links")
     try:
         authenticate_api()
         links = get_links(interval_minutes)
-        print("### number of matching links: " + str(len(links)))
+        log("### number of matching links: " + str(len(links)))
         handle_links(links)
     except Exception as e:
-        print("### something went wrong:")
-        print(str(e))
+        log("### something went wrong:")
+        log(str(e))
     finally:
         if driver:
             driver.quit()
             driver = None
-    print("### sleeping...")
+    log("### sleeping...")
     time.sleep(interval_minutes * 60)
